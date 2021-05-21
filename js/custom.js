84b8859eae1592b5febf27d2af73c6a5119fb821
Martina-Wei <xxxxxtsuki@gmail.com>
2021-05-20 17:57:33 -0700

    Update: project modal+resource

--- js/custom.js
+++ js/custom.js
@@ -104,7 +104,13 @@ $(document).ready(function () {
     });
 
     //animatedModal
-    $("#demo01,#demo02,#demo03,#demo04,#demo05,#demo06,#demo07,#demo08,#demo09").animatedModal();
+    $("#demo04,#demo05").animatedModal();
+    $("#demo01").animatedModal({
+        modalTarget: 'animatedModal1'
+    });
+    $("#demo03").animatedModal({
+        modalTarget: 'animatedModal3'
+    });
 
     // Contact Form 	
 
